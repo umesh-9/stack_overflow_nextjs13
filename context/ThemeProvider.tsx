0ffe9c0c12400dc009cae0a840a0a3c3dff401cd
umesh-9 <umeshnaidu00@gmail.com>
2023-10-23 09:37:05 +0530

    navbar created

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -12,17 +12,23 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(perfers-color-scheme:dark)").matches)
+    ) {
+      setMode("dark");
       document.documentElement.classList.add("dark");
     } else {
-      setMode("dark");
-      document.documentElement.classList.add("light");
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
   useEffect(() => {
     handleThemeChange();
   }, [mode]);
+  console.log("mmode", mode);
+
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
       {children}
